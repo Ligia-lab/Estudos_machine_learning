e164a9f589d4262d140ab7f4ce772b677e55a6c5
Ligia <ligiacamara92@gmail.com>
2025-10-15 17:35:54 -0300

    add pipeline

--- churn/train.py
+++ churn/train.py
@@ -60,7 +60,6 @@ X_train.isna().sum().sort_values(ascending=False)
 df_analise = X_train.copy()
 df_analise[target] = y_train
 summario = df_analise.groupby(by=target).agg(['mean', 'median']).T
-summario
 summario['diff_abs'] = summario[0] - summario[1]
 summario['diff_rel'] = summario[0] / summario[1]
 summario.sort_values(by=['diff_rel'], ascending=False)
@@ -74,27 +73,37 @@ import matplotlib.pyplot as plt
 arvore = tree.DecisionTreeClassifier(random_state=42)
 arvore.fit(X_train, y_train)
 
-# %%
+#%%
 
 #definição das features para serem utilizadas
-feature_importances = (pd.Series(arvore.feature_importances_, index=X_train.columns)
-                                .sort_values(ascending=False)
-                                .reset_index())
-feature_importances['acum'] = feature_importances[0].cumsum()
-feature_importances[feature_importances['acum'] > 0.96]
+feature_importances = (pd.Series(arvore.feature_importances_, 
+                                 index=X_train.columns)
+                            .sort_values(ascending=False)
+                            .reset_index())
+
+feature_importances['acum.'] = feature_importances[0].cumsum()
+feature_importances[feature_importances['acum.'] < 0.96]
 
  # %%
 
 #criação da variável só com as melhores features
-best_features = (feature_importances[feature_importances['acum'] > 0.96]['index'].to_list())
+best_features = (feature_importances[feature_importances['acum.'] < 0.96]['index']
+                 .to_list())
 
 best_features
+
+X_train[best_features]
 
 # %%
 
 #MODIFY
 
-from feature_engine import discretisation
+from feature_engine import discretisation, encoding
+from sklearn import pipeline
+
+
+
+#discretizar
 
 tree_discretization = discretisation.DecisionTreeDiscretiser(
     variables=best_features,
@@ -102,10 +111,25 @@ tree_discretization = discretisation.DecisionTreeDiscretiser(
     bin_output='bin_number',
     cv=3)
 
-tree_discretization.fit(X_train, y_train)
+# tree_discretization.fit(X_train[best_features], y_train)
 
-X_train_transform = tree_discretization.transform(X_train)
-X_train_transform
+# X_train_transform = tree_discretization.transform(X_train[best_features])
+
+
+#Onehot
+
+onehot = encoding.OneHotEncoder(variables=best_features, ignore_format=True)
+#onehot.fit(X_train_transform, y_train)
+
+# X_train_transform = onehot.transform(X_train_transform)
+# X_train_transform
+
+# arvore_nova = tree.DecisionTreeClassifier(random_state=42)
+# arvore_nova.fit(X_train_transform, y_train)
+
+# (pd.Series(arvore_nova.feature_importances_, index=X_train_transform.columns)
+#           .sort_values(ascending=False))
+
 
 # %%
 
@@ -114,7 +138,23 @@ X_train_transform
 from sklearn import linear_model
 
 reg = linear_model.LogisticRegression(penalty=None, random_state=42, max_iter=1000000)
-reg.fit(X_train_transform, y_train)
+#reg.fit(X_train_transform, y_train)
+
+
+#%%
+
+#PIPELINE
+
+model_pipeline = pipeline.Pipeline(
+    steps=[
+        ('discretizar', tree_discretization),
+        ('Onehot', onehot),
+        ('Model', reg),
+    ]
+)
+
+model_pipeline.fit(X_train, y_train)
+
 
 # %%
 
@@ -124,8 +164,8 @@ from sklearn import metrics
 
 
 #TREINO
-y_train_predict = reg.predict(X_train_transform)
-y_train_proba = reg.predict_proba(X_train_transform)[:,1]
+y_train_predict = model_pipeline.predict(X_train)
+y_train_proba = model_pipeline.predict_proba(X_train)[:,1]
 
 acc_train = metrics.accuracy_score(y_train, y_train_predict)
 auc_train = metrics.roc_auc_score(y_train, y_train_proba)
@@ -135,10 +175,11 @@ print('AUC treino: ', auc_train)
 
 
 #TESTE
-X_test_transform = tree_discretization.transform(X_test)
+# X_test_transform = tree_discretization.transform(X_test[best_features])
+# X_test_transform = onehot.transform(X_test_transform)
 
-y_test_predict = reg.predict(X_test_transform)
-y_test_proba = reg.predict_proba(X_test_transform)[:,1]
+y_test_predict = model_pipeline.predict(X_test)
+y_test_proba = model_pipeline.predict_proba(X_test)[:,1]
 
 acc_test = metrics.accuracy_score(y_test, y_test_predict)
 auc_test = metrics.roc_auc_score(y_test, y_test_proba)
@@ -148,10 +189,11 @@ print('AUC test: ', auc_test)
 
 
 #OOT
-oot_transform = tree_discretization.transform(oot[features])
+# oot_transform = tree_discretization.transform(oot[best_features])
+# oot_transform = onehot.transform(oot_transform)
 
-y_oot_predict = reg.predict(oot_transform)
-y_oot_proba = reg.predict_proba(oot_transform)[:,1]
+y_oot_predict = model_pipeline.predict(oot[features])
+y_oot_proba = model_pipeline.predict_proba(oot[features])[:,1]
 
 acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
 auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
@@ -161,3 +203,7 @@ print('AUC oot: ', auc_oot)
 
 
 # %%
+
+
+
+
